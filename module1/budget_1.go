69649ed661e5fef978c40d0053fd054afaa2ca05
Kristiana <kristiana.nedelcheva@sap.com>
2022-12-19 14:57:45 +0200

    adding structs

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -2,4 +2,15 @@ package module1
 
 // Budget stores budget information
 
+func main() {
+	type Budget struct {
+		Max  float32
+		Item []Item
+	}
+	type Item struct {
+		Description string
+		Price       float32
+	}
+}
+
 // Item stores item information
