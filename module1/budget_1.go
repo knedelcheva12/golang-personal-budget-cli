f33fd587f3c0fcfc23b67afdbba7f4d0099b81b5
knedelcheva12 <krisinedel98@gmail.com>
2022-12-19 15:13:25 +0200

    adding structs

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -2,15 +2,14 @@ package module1
 
 // Budget stores budget information
 
-func main() {
-	type Budget struct {
-		Max  float32
-		Item []Item
-	}
-	type Item struct {
-		Description string
-		Price       float32
-	}
+type Budget struct {
+	Max  float32
+	Item []Item
 }
 
 // Item stores item information
+
+type Item struct {
+	Description string
+	Price       float32
+}
